2a89944bb2629080b3a484e636a484535e7063ec
vishwajeet-7 <vishady7@gmail.com>
2023-03-29 17:08:01 +0530

    updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,107 @@
-import React from 'react'
+import React from "react";
+import { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {
+    const {name,value} = e.target;
+    setForm({...form,[name]:value})
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    // default template id : template_tcsfv4i
+    //Service id: service_l8nmjy5
+    //public key: Puffb7nBRy0x0xN5d
+    emailjs.send("service_l8nmjy5","template_tcsfv4i",{
+      from_name:form.name,
+      to_name:"Vishwajeet",
+      from_email:form.email,
+      to_email:"vishady7@gmail.com",
+      message:form.message
+    },"Puffb7nBRy0x0xN5d")
+    .then(()=>{
+      setLoading(false);
+      alert("Thank you, I will get back to you as soon as possible.");
 
-export default Contact
+      setForm({
+        name:"",
+        email:"",
+        message:"",
+      })
+    },(error)=>{
+      setLoading(false)
+      console.log(error);
+      alert("Something went wrong, email not sent")
+    })
+  };
+  return (
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+      >
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+        >
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Name</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your name?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Email</span>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="What's your email?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Message</span>
+            <textarea
+              rows={"7"}
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              placeholder="Type your message here"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+            />
+          </label>
+          <button type="submit" className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl">{loading ? "sending..." : "Send"}</button>
+        </form>
+      </motion.div >
+
+      <motion.div variants={slideIn("right", "tween", 0.2, 1)} className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]">
+      <EarthCanvas/>
+      </motion.div>
+    </div>
+  );
+};
+
+export default SectionWrapper(Contact, "contact");
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -23,8 +23,19 @@ const ExperienceCard = ({exp})=>(
   }
   >
     <div>
-      <h3 className="text-white text-[24px] font-bold">{exp.title}</h3>
+      <h3 className="text-white text-[24px] font-bold">{exp.company_name}</h3>
+      <p className="text-secondary text-[16px] font-semibold" style={{margin:0}}>{exp.title}</p>
     </div>
+    <ul className="mt-5 list-disc ml-5 space-y-2">
+      {
+        exp.points.map((point,index)=>(
+          <li key={`exp-point-${index}`}
+          className="text-white-100 text-[14px] pl-1 tracking-wider">
+            {point}
+          </li>
+        ))
+      }
+    </ul>
   </VerticalTimelineElement>
 )
 
